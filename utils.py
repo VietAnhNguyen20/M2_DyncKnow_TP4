a63684b5be41b1ed05696e54e49241c0132ba07c
NGUYEN Viet Anh <nguyenvietanh101010@gmail.com>
2023-12-06 11:42:58 +0100

    Done splitting

--- utils.py
+++ utils.py
@@ -1,3 +1,24 @@
+import random as rand
 
+#Fix the randomness
+rand.seed(1234)
 
-def split_data(ratio_split = 0.2):
+def split_data(input_file, output_file_1, output_file_2, ratio_split=0.2):
+    with open(input_file, 'r') as file:
+        data_lines = file.readlines()
+
+    total_lines = len(data_lines)
+    split_index = int(total_lines * (1 - ratio_split))
+
+    rand.shuffle(data_lines)
+
+    set_1 = data_lines[:split_index]
+    set_2 = data_lines[split_index:]
+
+    with open(output_file_1, 'w') as file_1:
+        file_1.writelines(set_1)
+
+    with open(output_file_2, 'w') as file_2:
+        file_2.writelines(set_2)
+
+    return None
